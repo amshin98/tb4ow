22ffea0bc2e5b7a4e04e23a722e86356302239f4
Austin Shin <23241280+amshin98@users.noreply.github.com>
2022-03-09 11:39:14 -0800

    Fix fish direction

--- TB4OW/Assets/Scripts/RangedController.cs
+++ TB4OW/Assets/Scripts/RangedController.cs
@@ -21,7 +21,7 @@ public class RangedController : WeaponController
     override public void UseWeapon()
     {
         // Shoot projectile
-        //TODO FIX THE FISH DIRECTION
+        //TODO FIX THE FISH DIRECTION   
         var xOffset = transform.parent.rotation.y == 0 ? X_OFFSET_R : X_OFFSET_L;
         Instantiate(projectile,
             transform.position + new Vector3(xOffset, Y_OFFSET, 0), transform.rotation);
